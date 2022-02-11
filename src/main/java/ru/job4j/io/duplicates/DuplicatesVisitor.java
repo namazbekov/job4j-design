477c961b925267730ed269365c9c67b2aa5194da
Дасан <dastan.namazbekov@mail.ru>
2022-02-11 17:40:21 +0300

    4.2. Поиск дубликатов [#315066 #259713]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,20 +5,34 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Objects;
+import java.util.List;
+import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    HashMap<Path, Path> map = new HashMap<>();
-
+    HashMap<FileProperty, List<Path>> map = new HashMap<>();
+    List<Path> listDuplicate = new ArrayList<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty value = new FileProperty(file.toFile().length(), file.getFileName().toString());
-        if (Objects.equals(value.getName(), file.getFileName().toString())
-                && value.getSize() == file.toString().length()) {
-            map.put(file.getFileName(), file.toAbsolutePath());
-            System.out.println(map);
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        if (map.containsKey(fileProperty)) {
+            listDuplicate.add(file);
+            map.put(fileProperty, listDuplicate);
+        } else {
+            List<Path> firstIn = new ArrayList<>();
+            firstIn.add(file);
+            map.put(fileProperty, firstIn);
         }
         return super.visitFile(file, attrs);
+    }
+    public List<Path> searchSizeMoreOne() {
+        List<Path> list = new ArrayList<>();
+        for (Map.Entry<FileProperty, List<Path>> collect : map.entrySet()) {
+            if (map.size() > 1) {
+                list.addAll(collect.getValue());
+            }
+        }
+        return list;
     }
 }
